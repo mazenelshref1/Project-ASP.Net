3f953f17dd08439bcf0a3f08cfa482be8ddec222
‪Mazen ‬‏ <mazensouliman500@gmail.com>
2026-03-30 17:40:59 +0200

    finish student CRUD

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,26 +1,94 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StudentSystem.Models;
+using StudentSystem.ViewModels;
 
 namespace StudentSystem.Controllers
 {
     public class StudentController : Controller
     {
         AppDbContext db = new AppDbContext();
-
+        AppDbContext context = new AppDbContext(); 
         public IActionResult ShowAll()
         {
-            var students = db.Students.ToList();
+            var students = context.Students.ToList();
             return View(students);
         }
 
         public IActionResult ShowDetails(int id)
         {
-            var student = db.Students
+            var students = context.Students
                 .Include(s => s.Department)
                 .FirstOrDefault(s => s.Id == id);
 
-            return View(student);
+            return View(students);
+        }
+        public IActionResult GetAll()
+        {
+            var students = context.Students.Include(s => s.Department).ToList();
+            return View(students);
+        }
+        public IActionResult GetById(int id)
+        {
+            var students = context.Students.Include(s => s.Department).FirstOrDefault(s => s.Id == id);
+            return View(students);
+        }
+        public IActionResult Add()
+        {
+            StudentWithDeptListVM vm = new StudentWithDeptListVM();
+            vm.Departments = context.Departments.ToList();
+            return View(vm);
+        }
+        [HttpPost]
+        public IActionResult Add(StudentWithDeptListVM vm)
+        {
+            Student student = new Student();
+            student.Name = vm.Name;
+            student.Age = vm.Age;
+            student.DepartmentId = vm.DepartmentId;
+            context.Students.Add(student);
+            context.SaveChanges();
+            return RedirectToAction("GetAll");
+        }
+        public IActionResult Edit(int id)
+        { 
+            Student? student = context.Students.FirstOrDefault(s => s.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            StudentWithDeptListVM vm = new StudentWithDeptListVM();
+            vm.Id = student.Id;
+            vm.Name = student.Name;
+            vm.Age = student.Age;
+            vm.DepartmentId = student.DepartmentId;
+            vm.Departments = context.Departments.ToList();
+            return View(vm);
+        }
+        [HttpPost]
+        public IActionResult Edit(StudentWithDeptListVM vm)
+        { 
+            Student? student = context.Students.FirstOrDefault(s => s.Id == vm.Id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            student.Name = vm.Name;
+            student.Age = vm.Age;
+            student.DepartmentId = vm.DepartmentId;
+            context.SaveChanges();
+            return RedirectToAction("GetAll");
+        }
+        public ActionResult Delete(int id)
+        { 
+            Student? student = context.Students.FirstOrDefault(s => s.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            context.Students.Remove(student);
+            context.SaveChanges();
+            return RedirectToAction("GetAll");
         }
     }
 }
---
+++ ViewModels/StudentWithDeptListVM.cs
@@ -0,0 +1,13 @@
+﻿using StudentSystem.Models;
+
+namespace StudentSystem.ViewModels
+{
+    public class StudentWithDeptListVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int DepartmentId { get; set; }
+        public List<Department> Departments { get; set; }
+    }
+}
